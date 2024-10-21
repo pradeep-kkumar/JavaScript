7ea0e90a27597742d3499caf0fcf8ddd288369dc
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-10-21 08:00:40 +0000

    JS

---
+++ Notes.txt/script.js
@@ -0,0 +1,8 @@
+// This is the basic syntax of JavaScript
+console.log("This is the first code of JS")
+
+// function of js
+function myFun () {
+    document.getElementById('demo')
+        .innerHTML = 'Content Changed'
+}
