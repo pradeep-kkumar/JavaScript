9079643c5f5a621b391707cde410e67f584b2ef2
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-10-30 07:18:36 +0000

    JavaScript Variable

---
+++ 01_Basic/Variable.js
@@ -0,0 +1,19 @@
+const emp_id = "1231"  // this is a constant variable we can be never change it.
+
+// emp_id = 12 // not allowed 
+
+let emp_name = "John" 
+
+var emp_email = "john@gmail.com"  // it is also a variable type but it is not used in those days
+
+emp_phone = "9876789878"
+
+let emp_city; // it undefined variable 
+
+// How to print the statement
+
+console.log(emp_id)
+
+// If we want to print out put in table form 
+
+console.table([emp_id, emp_name, emp_phone, emp_email, emp_city]);
