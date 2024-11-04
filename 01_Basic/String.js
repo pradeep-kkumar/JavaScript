90efd393002dbc8339c46baf5782799c51d97a7d
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-11-04 17:17:30 +0000

    JS

---
+++ 01_Basic/String.js
@@ -0,0 +1,23 @@
+// String:- The JavaScript string is an object that represents a sequence of characters.
+
+// There are 2 ways to create string in JavaScript
+
+// 1. By string literal
+// 2. By string object (using new keyword)
+
+// 1) By string literal:- The string literal is created using double quotes. The syntax of creating string 
+// using string literal is given below:
+
+let name = "Punit";
+
+console.log(name)
+console.log(typeof name) //  the data type  of this string is string
+
+// 2) By string object (using new keyword)
+// The syntax of creating string object using new keyword is given below:
+
+let otherName = new String("Other name is Pradeep");
+
+console.log(otherName)
+console.log(typeof otherName) // the data type of this String is Object type
+
