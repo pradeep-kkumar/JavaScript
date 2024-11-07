d762963933b412f5225e50c1a3d7d8ae7917731f
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-11-07 09:35:43 +0000

    JS

---
+++ 01_Basic/DataTypes.js
@@ -0,0 +1,42 @@
+// JavaScript has 8 Datatypes
+
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {
+    firstName:"John", 
+    lastName:"Doe"
+};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+// **********************************************************************************************************************
+
+let a = 16 + "Volvo";
+console.log(a) // 16Volvo
+
+let b = "Car" + 16
+console.log(a) 
