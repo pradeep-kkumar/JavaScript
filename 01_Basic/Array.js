f0a250bdc8d489ca8739d2587a0e065225562b34
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-11-09 08:46:24 +0000

    JS

---
+++ 01_Basic/Array.js
@@ -0,0 +1,39 @@
+// Array :- An array is a special variable, which can hold more than one value.
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+// console.log(myArr[1]);
+
+const myHeros = ["bmw", "audi", "suzuki"];
+
+// console.log(myHeros);
+
+// Array Methods
+
+// myArr.push(6) // add 6
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop() // remove last element
+// console.log(myArr);
+
+// myArr.unshift(9) // add element in the starting
+// console.log(myArr);
+// myArr.shift() // remove the stating element
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+
+// console.log(myArr);
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+
+
+
+
+
+
