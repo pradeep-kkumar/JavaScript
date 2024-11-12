df8fa1036e208918d258c4524c7677289ba28c30
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-11-12 08:40:20 +0000

    json

---
+++ 02_Basic/03_Object.js
@@ -0,0 +1,39 @@
+const course = {
+    coursename : "JavaScript",
+    price : "999",
+    teacher : "hitesh"
+}
+
+// console.log(course.teacher);
+
+// const {teacher} = course
+const {teacher : instructor} = course
+
+// console.log(teacher);
+// console.log(instructor);
+
+// for react ******************************
+
+// const navbar = ({company}) => {
+
+// }
+
+// const navbar(company = "HCL")
+
+// *****************************************
+
+// JSON
+ 
+// {
+//     "name" : "Pradeep",
+//     "coursename" : "JAvaScript",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
