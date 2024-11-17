dcb072976ad946859d208fc1cd5bb4eeb9dd80db
Pradeep Kumar <111398718+pradeep-kkumar@users.noreply.github.com>
2024-11-17 19:20:14 +0000

    JS

---
+++ 04_Control_flow/One.js
@@ -0,0 +1,53 @@
+// if 
+
+// if (true){
+//     // Ander ka code execute hoga
+// }
+
+// if (false){
+//     // Ander ka code execute nhi hoga
+// }
+
+// const inUserLoggedIn = true
+
+// if (inUserLoggedIn){
+//     console.log("Welcome, you are logged in");
+// }
+
+if (3 != 2 ){
+    // console.log("Both are not equal");
+}
+
+if (3 == "3"){
+    // console.log("Executed");
+}
+
+if (3 === "3"){
+    // console.log("Executed");
+}
+
+if (3 != 2){  // ye ulta kaam krta hai jaise = ka not = or not = ka =
+    // console.log("Executed");
+}
+
+
+// const temp = 41
+
+// if (temp === 41){
+//     console.log("Less than 50");    
+// } else {
+//     console.log("temperature is greater than 50");    
+// }
+
+
+
+const age = 23;
+
+if (age >= 18){
+    console.log("This person is eligibale for botting");    
+} else {
+    console.log("This person is not be eligibale for botting");    
+}
+
+
+// <, >, <=, >=, ==, === (this is also check the data type also), !=, 
